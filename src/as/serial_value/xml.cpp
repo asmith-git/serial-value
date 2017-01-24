f55e436bc73de59da46b11103d92795c1a537995
SolaireLibrary <solairelibrary@mail.com>
2017-01-24 21:49:22 +0000

    Implemented write_element

--- src/as/serial_value/xml.cpp
+++ src/as/serial_value/xml.cpp
@@ -54,24 +54,65 @@ void write_element(std::ostream& aStream, const std::string& aName, const as::se
 	//! \todo Remove chars illegal in XML
 	switch (aValue.get_type()) {
 	case as::serial_value::NULL_T:
+		aStream << '<' << aName << '/' << '>';
 		break;
 	case as::serial_value::CHAR_T:
+		aStream << '<' << aName << '>' << aValue.get_char() << '<' << '/' << aName << '>';
 		break;
 	case as::serial_value::BOOL_T:
+		aStream << '<' << aName << '>' << (aValue.get_bool() ? "true" : "false") << '<' << '/' << aName << '>';
 		break;
 	case as::serial_value::UNSIGNED_T:
+		aStream << '<' << aName << '>' << aValue.get_unsigned() << '<' << '/' << aName << '>';
 		break;
 	case as::serial_value::SIGNED_T:
+		aStream << '<' << aName << '>' << aValue.get_signed() << '<' << '/' << aName << '>';
 		break;
 	case as::serial_value::FLOAT_T:
+		aStream << '<' << aName << '>' << aValue.get_float() << '<' << '/' << aName << '>';
 		break;
 	case as::serial_value::POINTER_T:
+		aStream << '<' << aName << '>' << ASMITH_SERIAL_PTR << aValue.get_pointer() << '<' << '/' << aName << '>';
 		break;
 	case as::serial_value::STRING_T:
+		aStream << '<' << aName << '>' << aValue.get_string() << '<' << '/' << aName << '>';
 		break;
 	case as::serial_value::ARRAY_T:
+		{
+			aStream << '<' << aName << '>';
+			const as::serial_value::array_t& array_ = aValue.get_array();
+			const size_t s = array_.size();
+			for(size_t i = 0; i < s; ++i) write_element(aStream, std::to_string(i), array_[i]);
+			aStream << '<' << '/' << aName << '>';
+		}
 		break;
 	case as::serial_value::OBJECT_T:
+		{
+			const as::serial_value::object_t& object = aValue.get_object();
+			aStream << '<' << aName;
+			size_t attributes = 0;
+			for(const auto& i : object) {
+				const as::serial_value::type t = i.second.get_type();
+				if(t != as::serial_value::ARRAY_T && t != as::serial_value::OBJECT_T) {
+					write_attribute(aStream, i.first, i.second);
+					++attributes;
+				}
+			}
+
+			if(attributes == object.size()) {
+				aStream << '/' << '>';
+			}else {
+				aStream << '>';
+				for(const auto& i : object) {
+					const as::serial_value::type t = i.second.get_type();
+					if(t == as::serial_value::ARRAY_T || t == as::serial_value::OBJECT_T) {
+						write_element(aStream, i.first, i.second);
+						++attributes;
+					}
+				}
+				aStream << '<' << '/' << aName << '>';
+			}
+		}
 		break;
 	}
 }
